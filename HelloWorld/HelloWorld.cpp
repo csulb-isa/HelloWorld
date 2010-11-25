f38ace1bec8a0d31fbca5445995f33e664170169
sg- <sgrove@csulb.edu>
2010-11-25 11:15:08 -0800

    This project will be useful for others learning how to use all the features of git.  First commit

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// write to the console
+	printf("Hello World!");
+	// halt the program until close is requested
+	while(1);
+	// standard return eop
+	return 0;
+}
+
